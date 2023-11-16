f0f64e0c41a9b2daebb8ee7a5b3e7a7b167f7185
totsumaru <techstart35@gmail.com>
2023-11-16 23:24:47 +0900

    stateをClientに移動

--- app/server/[serverId]/Client.tsx
+++ app/server/[serverId]/Client.tsx
@@ -2,17 +2,34 @@
 
 import { Container, Text } from '@chakra-ui/react'
 import Header from "@/components/Header";
-import React from "react";
-import DiscordEmbedUI from "@/components/DiscordEmbedUI";
+import React, { useState } from "react";
+import DiscordEmbedUI from "@/components/embed/DiscordEmbedUI";
 
 export default function Client() {
+  const [panelTitle, setPanelTitle] = useState<string>("")
+  const [panelDescription, setPanelDescription] = useState<string>("")
+  const [panelImage, setPanelImage] = useState<string>("")
+  const [panelButtonLabel, setPanelButtonLabel] = useState<string>("ガチャを引く")
+  const [panelButtonColor, setPanelButtonColor] = useState<string>("blue")
+
   return (
     <>
       <Header/>
       <Container mt={10}>
         <Text fontSize='xl'>パネル</Text>
         {/* パネル */}
-        <DiscordEmbedUI/>
+        <DiscordEmbedUI
+          title={panelTitle}
+          setTitle={(title) => setPanelTitle(title)}
+          description={panelDescription}
+          setDescription={(description) => setPanelDescription(description)}
+          image={panelImage}
+          setImage={(image) => setPanelImage(image)}
+          buttonLabel={panelButtonLabel}
+          setButtonLabel={(buttonLabel) => setPanelButtonLabel(buttonLabel)}
+          buttonColor={panelButtonColor}
+          setButtonColor={(buttonColor) => setPanelButtonColor(buttonColor)}
+        />
       </Container>
     </>
   )
--- components/embed/DiscordEmbedUI.tsx
+++ components/embed/DiscordEmbedUI.tsx
@@ -1,38 +1,54 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Box, Center, HStack, IconButton, Image, Input, Textarea, VStack } from '@chakra-ui/react';
-import ButtonDrawer from "@/components/embed/button";
+import ButtonDrawer from "@/components/embed/Button";
 import { FaTrashAlt } from "react-icons/fa";
 
-function DiscordEmbedUI() {
-  const [imageSrc, setImageSrc] = useState('');
-  const [buttonLabel, setButtonLabel] = useState('ガチャを引く');
-  const [buttonColor, setButtonColor] = useState('blue');
+type Props = {
+  title: string;
+  setTitle: (title: string) => void;
+  description: string;
+  setDescription: (description: string) => void;
+  image: string;
+  setImage: (image: string) => void;
+  buttonLabel: string;
+  setButtonLabel: (buttonLabel: string) => void;
+  buttonColor: string;
+  setButtonColor: (buttonColor: string) => void;
+}
+
+/**
+ * DiscordのEmbedUIを再現したコンポーネントです
+ */
+function DiscordEmbedUI(props: Props) {
   const bg = 'gray.800'; // Discordのダークモードに近い背景色
   const borderColor = 'gray.600';
   const textColor = 'gray.100'; // 明るいテキストカラー
   const placeholderColor = 'gray.500'; // プレースホルダーのカラー
 
+  // 画像を変更します
   const handleImageChange = (e: any) => {
     const file = e.target.files[0];
     if (file) {
-      setImageSrc(URL.createObjectURL(file));
+      props.setImage(URL.createObjectURL(file));
     }
   };
 
+  // 画像を削除します
   const handleImageDelete = () => {
-    setImageSrc(''); // 画像URLをリセット
+    props.setImage('');
   };
 
   return (
     <Box bg={bg} p={4} borderRadius="md" maxW="xl" mx="auto" color={textColor}>
       <VStack spacing={4} align="stretch">
 
-        {/* Title Input */}
+        {/* タイトル */}
         <Input
           placeholder="タイトル"
           variant="filled"
-          _placeholder={{ color: placeholderColor }}
+          onChange={(e) => props.setTitle(e.target.value)}
           bg={bg}
+          _placeholder={{ color: placeholderColor }}
           _hover={{ bg: 'gray.700' }} // hover時の背景色を追加
         />
 
@@ -41,8 +57,9 @@ function DiscordEmbedUI() {
           placeholder="メッセージをここに記入"
           variant="filled"
           size="md"
-          _placeholder={{ color: placeholderColor }}
+          onChange={(e) => props.setDescription(e.target.value)}
           bg={bg}
+          _placeholder={{ color: placeholderColor }}
           _hover={{ bg: 'gray.700' }} // hover時の背景色を追加
         />
 
@@ -68,8 +85,8 @@ function DiscordEmbedUI() {
               cursor="pointer"
               onChange={handleImageChange}
             />
-            {imageSrc ? (
-              <Image src={imageSrc} alt="Uploaded image" borderRadius="md"/>
+            {props.image ? (
+              <Image src={props.image} alt="Uploaded image" borderRadius="md"/>
             ) : (
               <Center p={12}>
                 クリックして画像を選択
@@ -78,7 +95,7 @@ function DiscordEmbedUI() {
           </Box>
 
           {/* Delete Image Icon */}
-          {imageSrc && (
+          {props.image && (
             <IconButton
               icon={<FaTrashAlt/>}
               aria-label="Delete image"
@@ -94,10 +111,10 @@ function DiscordEmbedUI() {
         {/* 'ガチャを引く' Button */}
         <HStack justifyContent="flex-start">
           <ButtonDrawer
-            label={buttonLabel}
-            setLabel={(label) => setButtonLabel(label)}
-            color={buttonColor}
-            setColor={(color) => setButtonColor(color)}
+            label={props.buttonLabel}
+            setLabel={(label) => props.setButtonLabel(label)}
+            color={props.buttonColor}
+            setColor={(color) => props.setButtonColor(color)}
           />
         </HStack>
 
